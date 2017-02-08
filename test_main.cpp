11dc21939c4f532dbbfcf17d435a1b8c45d26e14
Ian Howell <ian.howell0@gmail.com>
2017-02-08 16:28:07 -0600

    Add initial test files

---
+++ test_main.cpp
@@ -0,0 +1,6 @@
+// Name:        Ian Howell
+// Class:       CS 5201 - Object-Oriented Numerical Modeling
+// Date:        February 10, 2017
+
+#define BOOST_TEST_MODULE "main"
+#include<boost/test/included/unit_test.hpp>
---
+++ test_medianheap.cpp
@@ -0,0 +1,42 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+#include "medianheap.h"
+
+BOOST_AUTO_TEST_SUITE(medianheap_suite)
+
+/* Default Constructor Test */
+BOOST_AUTO_TEST_CASE(default_constructor)
+{
+  BOOST_CHECK(false);
+}
+
+/* Copy Constructor Test */
+BOOST_AUTO_TEST_CASE(copy_constructor)
+{
+  BOOST_CHECK(false);
+}
+
+/* Assignment Operator test */
+BOOST_AUTO_TEST_CASE(assignment_operator)
+{
+  BOOST_CHECK(false);
+}
+
+/* Equality Operator */
+BOOST_AUTO_TEST_CASE(equality_operator)
+{
+  BOOST_CHECK(false);
+}
+ 
+/* Non-Equality Operator */
+BOOST_AUTO_TEST_CASE(nonequality_operator)
+{
+  BOOST_CHECK(false);
+}
+
+BOOST_AUTO_TEST_CASE(out_of_bounds_exception)
+{
+  BOOST_CHECK(false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
